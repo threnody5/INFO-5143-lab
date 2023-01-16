03c7365498b7e1ecb582f8c3da9a3b8d4133326d
William <williamw15@protonmail.com>
2023-01-15 21:17:04 -0500

    remove tester code, add Header and Tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+/** @format */
+
 import './App.css';
+
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div>
-      <h2>App works</h2>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Tasks />
+      </div>
     </div>
   );
 }
