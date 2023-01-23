81dfc5c45e9624e8d00809f66776f30d6d3bd722
William <williamw15@protonmail.com>
2023-01-22 22:34:36 -0500

    add tracker for tasks remaining, open tasks and completed tasks

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import Task from './Task';
@@ -26,6 +26,23 @@ export default function Tasks() {
     },
   ]);
 
+  const [countTasks, setCountTasks] = useState();
+  const [openStatusCount, setOpenStatusCount] = useState();
+  const [completedStatusCount, setCompletedStatusCount] = useState();
+
+  let displayedTasks;
+
+  useEffect(() => {
+    const currentTasks = tasks.filter((task) => task).length;
+    setCountTasks(currentTasks);
+    const openTasks = tasks.filter((task) => task.status === 'Open').length;
+    setOpenStatusCount(openTasks);
+    const completedTasks = tasks.filter(
+      (task) => task.status === 'Completed'
+    ).length;
+    setCompletedStatusCount(completedTasks);
+  });
+
   const setTaskHandler = (id) => {
     const updatedTask = [...tasks];
     updatedTask.forEach((selectedTask) => {
@@ -49,23 +66,40 @@ export default function Tasks() {
     setTasks(clearedTasks);
   };
 
+  if (countTasks > 1) {
+    displayedTasks = `There are ${countTasks} tasks remaining`;
+  } else if (countTasks == 0) {
+    displayedTasks = `There are no tasks remaining`;
+  } else {
+    displayedTasks = `There is ${countTasks} task remaining`;
+  }
+
   return (
     <>
       <div className='container-fluid mt-3 mb-3'>
         <h1 style={{ fontWeight: 'bold', textAlign: 'center' }}>
-          These are the tasks
+          {displayedTasks}
         </h1>
+        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
+          Open Tasks: {openStatusCount}
+        </h3>
+        <h3 style={{ fontWeight: 'bold', textAlign: 'center' }}>
+          Completed Tasks: {completedStatusCount}
+        </h3>
       </div>
       <div className='container'>
-        <div 
-        style={{ display: 'flex', flexDirection: 'row', gap: 20, justifyContent: 'center' }}
-        className='mr-3'
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 20,
+            justifyContent: 'center',
+          }}
+          className='mr-3'
         >
           {tasks.map((mappedTask, key) => {
             return (
-              <div 
-              key={key}
-              >
+              <div key={key}>
                 <Task
                   key={key}
                   id={mappedTask.id}
@@ -81,7 +115,6 @@ export default function Tasks() {
         <div style={{ justifyContent: 'center', textAlign: 'center' }}>
           <button
             className='btn btn-warning mb-5'
-            // style={{ marginLeft: 150 }}
             onClick={() => {
               clearTasksHandler();
             }}
