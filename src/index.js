c4f23a99d0d588742a81d5621f227e7489e818ad
William <williamw15@protonmail.com>
2023-02-24 16:04:23 -0500

    wrap app with ThemeProvider comtext component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/styles.scss';
 import App from './App';
+import { ThemeProvider } from './components/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
