daeaca4ef2d25cb53da3fdeb84dcd60ce78fff5c
William <williamw15@protonmail.com>
2023-02-24 18:58:49 -0500

    task component supports both light and dark themes

--- src/components/Tasks/Task/index.jsx
+++ src/components/Tasks/Task/index.jsx
@@ -1,6 +1,10 @@
 /** @format */
 
-import './styles.scss';
+import { useContext } from 'react';
+
+import { ThemeContext } from '../../ThemeContext';
+
+import styles from './styles.module.scss';
 
 import Card from '../../Card';
 
@@ -8,44 +12,47 @@ import Card from '../../Card';
  * Component that renders the Task.
  */
 export default function Task({ id, title, status, setTask, removeTask }) {
+  const { theme } = useContext(ThemeContext);
   return (
-    <Card className='task-container'>
-      <div className='title'>
-        <h3>{title.toUpperCase()}</h3>
-      </div>
-      <div className='task-info'>
-        <strong>ID: </strong>
-        <span className='task-id'> {id}</span>
-      </div>
-      <div className='task-info'>
-        <strong>Status: </strong>
-        {/* Task status color changes depending on status state */}
-        <span
-          className='task-status'
-          style={{ color: status === 'Completed' ? '#28a745' : '#ffc107' }}
-        >
-          {status}
-        </span>
-      </div>
-      <div className='button-container'>
-        {/* Button for changing the status between 'Completed' and 'Open' */}
-        <button
-          className='status-button'
-          onClick={() => {
-            setTask(id);
-          }}
-        >
-          Change Status
-        </button>
-        {/* Button to remove the task from the list of tasks */}
-        <button
-          className='remove-button'
-          onClick={() => {
-            removeTask(id);
-          }}
-        >
-          Remove Task
-        </button>
+    <Card className={`${styles.taskContainer}`}>
+      <div className={`${styles[theme]}`}>
+        <div className={`${styles.titleContainer}`}>
+          <h3 className={`${styles.titleText}`}>{title.toUpperCase()}</h3>
+        </div>
+        <div className={`${styles.taskInfo}`}>
+          <strong>ID: </strong>
+          <span className={`${styles.taskId}`}> {id}</span>
+        </div>
+        <div className={`${styles.taskInfo}`}>
+          <strong>Status: </strong>
+          {/* Task status color changes depending on status state */}
+          <span
+            className={`${styles.taskStatus}`}
+            style={{ color: status === 'Completed' ? '#28a745' : '#ffc107' }}
+          >
+            {status}
+          </span>
+        </div>
+        <div className={`${styles.buttonContainer}`}>
+          {/* Button for changing the status between 'Completed' and 'Open' */}
+          <button
+            className={`${styles.statusButton}`}
+            onClick={() => {
+              setTask(id);
+            }}
+          >
+            Change Status
+          </button>
+          {/* Button to remove the task from the list of tasks */}
+          <button
+            className={`${styles.removeButton}`}
+            onClick={() => {
+              removeTask(id);
+            }}
+          >
+            Remove Task
+          </button>
+        </div>
       </div>
     </Card>
   );
