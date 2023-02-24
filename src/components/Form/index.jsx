f77e46940bcb5af3cc75465430ecb2f703de270e
William <williamw15@protonmail.com>
2023-02-23 23:42:28 -0500

    form component styling complete

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -21,65 +21,70 @@ export default function Form({
   };
 
   return (
-    <div>
-      <form>
-        <div>
-          <h2>Add a new task: </h2>
-          {/* Conditional rendering for displaying the list of errors to 
+    <form className='form-container'>
+      <div>
+        <h2>ADD A NEW TASK: </h2>
+        {/* Conditional rendering for displaying the list of errors to 
           the users if the errorMessage array has a length greater than 0 */}
-          {errorMessage.length > 0 && (
-            <div>
-              Missing Data:
-              <ul>
-                {errorMessage.map((error, index) => (
-                  <li key={index}>{error}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
+        {errorMessage.length > 0 && (
+          <div className='error-form'>
+            Missing Data:
+            <ul>
+              {errorMessage.map((error, index) => (
+                <li key={index}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <div>
         <div>
-          <div>
-            <label>
-              {/* Field that allows the user to enter in the description 
+          <label className='form-description'>
+            {/* Field that allows the user to enter in the description 
               that they desire for the task they're adding */}
-              Description:
-              <input
-                type='text'
-                placeholder='Description here'
-                value={description}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-            </label>
-            <span>
-              <label>
-                {/* Dropdown menu that allows the user to select 
+            {/* Description: */}
+            <input
+              title='Enter a description.'
+              type='text'
+              placeholder='Description here'
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </label>
+          <span>
+            <label>
+              {/* Dropdown menu that allows the user to select 
                 if the task they're adding is 'Open' or 'Completed */}
-                Status:
-                <select
-                  value={selectedValue}
-                  onChange={handleChange}
-                >
-                  {statusList.map((item, index) => (
-                    <option
-                      key={index}
-                      value={item.value}
-                    >
-                      {item.status}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </span>
-            <div>
-              {/* Button to add the task to the task list */}
-              <button onClick={addTask}>Add</button>
-            </div>
+              {/* Status: */}
+              <select
+                title='Choose an option.'
+                value={selectedValue}
+                onChange={handleChange}
+              >
+                {statusList.map((item, index) => (
+                  <option
+                    key={index}
+                    value={item.value}
+                  >
+                    {item.status}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </span>
+          <div>
+            {/* Button to add the task to the task list */}
+            <button
+              className='add-button'
+              onClick={addTask}
+            >
+              Add
+            </button>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 }
