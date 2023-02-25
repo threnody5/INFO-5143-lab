756424dbf6061a3ad5cc83586d8a078cb19e6264
William <williamw15@protonmail.com>
2023-02-24 22:39:06 -0500

    style theme button

--- src/components/ThemeToggle/index.jsx
+++ src/components/ThemeToggle/index.jsx
@@ -3,15 +3,20 @@
 import { useContext } from 'react';
 
 import { ThemeContext } from '../ThemeContext';
-
-import './styles.scss';
+import styles from './styles.module.scss';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   return (
-    <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-      {theme}
-    </button>
+    <div className={`${styles[theme]}`}>
+      <button
+        className={`${styles.themeButton}`}
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      >
+        {theme === 'light' ? <BsFillMoonStarsFill /> : <BsFillSunFill />}
+      </button>
+    </div>
   );
 };
 
