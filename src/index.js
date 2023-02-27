b35de4e0de92bae425ee38de8cc70cdaf67be680
William <williamw15@protonmail.com>
2023-02-26 19:42:59 -0500

    add background-color theme component wrapper around App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 /** @format */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/styles.scss';
+import styles from './styles/styles.module.scss';
 import App from './App';
-import { ThemeProvider } from './components/ThemeContext';
+import { ThemeProvider, ThemeContext } from './components/ThemeContext';
+
+const BackgroundManager = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+  return <div className={`${styles[theme]}`}>{children}</div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BackgroundManager>
+        <App />
+      </BackgroundManager>
     </ThemeProvider>
   </React.StrictMode>
 );
