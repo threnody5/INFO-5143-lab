1f5b99838c8e367c6d67f8394afc39202ed53195
William <williamw15@protonmail.com>
2023-02-26 22:48:52 -0500

    re-add scss file import to fix styling issues (why would you remove it in the first place?)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 // import styles from './styles/styles.module.scss';
+import './styles/styles.module.scss';
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
