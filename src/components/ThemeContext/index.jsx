7ae33c6c4bee1e61ad6bf3147b7ea18cb87b0a9a
William <williamw15@protonmail.com>
2023-02-28 17:48:18 -0500

    remove default value from createContext

--- src/components/ThemeContext/index.jsx
+++ src/components/ThemeContext/index.jsx
@@ -2,7 +2,7 @@
 
 import { createContext, useState } from 'react';
 
-export const ThemeContext = createContext({ theme: 'light' });
+export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
