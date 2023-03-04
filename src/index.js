63bb523f77eab130535f493620acaf24ee9ea0f1
William <williamw15@protonmail.com>
2023-03-03 21:24:59 -0500

    add react-redux, redux toolkit, comment out current useContext for theme, and create store

--- src/index.js
+++ src/index.js
@@ -5,27 +5,27 @@ import './styles/styles.module.scss';
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider, ThemeContext } from './components/ThemeContext';
+// import { ThemeProvider, ThemeContext } from './components/ThemeContext';
 
-const BackgroundManager = ({ children }) => {
-  const { theme } = useContext(ThemeContext);
-  return (
-    <div
-      theme={[theme]}
-      // className={`${styles[theme]}`}
-    >
-      {children}
-    </div>
-  );
-};
+// const BackgroundManager = ({ children }) => {
+//   const { theme } = useContext(ThemeContext);
+//   return (
+//     <div
+//       theme={[theme]}
+//       // className={`${styles[theme]}`}
+//     >
+//       {children}
+//     </div>
+//   );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BackgroundManager>
-        <App />
-      </BackgroundManager>
-    </ThemeProvider>
+    {/* <ThemeProvider> */}
+    {/* <BackgroundManager> */}
+    <App />
+    {/* </BackgroundManager> */}
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
