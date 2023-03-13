793b94236e69b9bf936cce67f3f5cf2adfe60476
William <williamw15@protonmail.com>
2023-03-13 12:22:29 -0400

    added remove task information to page for user, added styling

--- src/pages/Help/RemovingTasks/index.jsx
+++ src/pages/Help/RemovingTasks/index.jsx
@@ -1,7 +1,27 @@
 /** @format */
 
+import styles from './styles.module.scss';
+import { useSelector } from 'react-redux';
+
 const RemovingTasks = () => {
-  return <div>Removing Tasks works</div>;
+  const theme = useSelector((state) => state.theme.value);
+  return (
+    <main className={styles.removeTasks}>
+      <div className={styles[theme]}>
+        <h1>How to remove tasks</h1>
+        <ol>
+          <li>
+            Vel quam elementum pulvinar etiam. Ullamcorper morbi tincidunt
+            ornare massa.
+          </li>
+          <li>
+            Elementum pulvinar etiam non quam. Ullamcorper a lacus vestibulum
+            sed.
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 };
 
 export default RemovingTasks;
