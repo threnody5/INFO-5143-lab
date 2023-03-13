e13b1006c09bfae82b020fde60d60748eef591fd
William <williamw15@protonmail.com>
2023-03-13 11:29:23 -0400

    move Help content to HelpInformation page

--- src/pages/Help/index.jsx
+++ src/pages/Help/index.jsx
@@ -10,20 +10,7 @@ const Help = () => {
   return (
     <Card className={`${styles.helpContainer}`}>
       <div className={styles[theme]}>
-        <h1>Help</h1>
-        <h3>This App helps to keep track of your tasks.</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <div>
-          <Outlet />
-        </div>
+        <Outlet />
         <div>
           <Link to='adding-tasks'>Adding Tasks</Link>
         </div>
