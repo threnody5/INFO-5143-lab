d8cdc61ecc74d6ddc88ec5689405625d059169cc
William <williamw15@protonmail.com>
2023-03-13 10:40:07 -0400

    add task-counter to navbar

--- src/components/NavRouter/index.jsx
+++ src/components/NavRouter/index.jsx
@@ -11,6 +11,7 @@ const NavRouter = () => {
     <nav className={`${styles.navContainer} ${styles[theme]}`}>
       <NavLink to='/'>Tasks</NavLink>
       <NavLink to='add-task'>Add a Task</NavLink>
+      <NavLink to='task-counter'>Task Counter</NavLink>
       <NavLink to='help'>Help</NavLink>
     </nav>
   );
