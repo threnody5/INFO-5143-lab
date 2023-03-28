3b5c89598e9f31950d495916cbc6d1dc99e54a75
William <williamw15@protonmail.com>
2023-03-28 17:50:25 -0400

    install firebase package, create firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDqcy-mEFbR6vJtkmsD0m-J2Pv5x-oj_Ok',
+  authDomain: 'js4-todo-app.firebaseapp.com',
+  projectId: 'js4-todo-app',
+  storageBucket: 'js4-todo-app.appspot.com',
+  messagingSenderId: '568514379569',
+  appId: '1:568514379569:web:f007387fc5fd73205b3a10',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
